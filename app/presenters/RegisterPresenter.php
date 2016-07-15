d223b091f40a6ce3553ca19ba7ffcadfe1b72ff7
Vojtech Kotek <kotek.vojtech@gmail.com>
2016-07-16 00:17:49 +0200

    register shit broke

---
+++ app/presenters/RegisterPresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use App\Model;
+
+class RegisterPresenter extends BasePresenter 
+{    
+    protected function startup()
+    {
+        parent::startup();
+    }
+
+    protected function renderRegister()
+    {
+        
+    }
+}
