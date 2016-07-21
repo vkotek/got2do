78c83edeff1b89d3931c116aadee0dffc7fb856b
Vojtech Kotek <kotek.vojtech@gmail.com>
2016-07-21 18:31:24 +0200

    no idea

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -9,9 +9,18 @@ use App\Model;
 class HomepagePresenter extends BasePresenter
 {
 
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
 	public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		$this->template->users = $this->database->table('users')
+            ->order('id DESC')
+            ->limit(5);
 	}
 
 }
